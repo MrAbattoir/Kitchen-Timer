7985b315eb2ce02fa2658de71c7af0648872bb8d
Kevin Meatyard <kevinmeatyard@kevins-macbook-air.local>
2018-09-24 18:51:32 +0100

    conver to mvc

---
+++ Kitchen Timer/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Kitchen Timer
+//
+//  Created by Kevin Meatyard on 24/09/2018.
+//  Copyright © 2018 Kevin Meatyard. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
--- Kitchen Timer/TodoListController.swift
+++ Kitchen Timer/TodoListController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class TodoListController: UITableViewController {
     
-    var itemArray = ["Mike","Kevin", "Ash"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray=items
-        }
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Find Kevin"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Find Ash"
+        itemArray.append(newItem3)
+        
+        
+       if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+           itemArray = items
+       }
 
     }
 
@@ -31,7 +44,10 @@ class TodoListController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen whe...
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
@@ -57,21 +73,22 @@ class TodoListController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-
-        cell.textLabel?.text = itemArray[indexPath.row]
-
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //ternary operation
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
